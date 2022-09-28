1d078db08094e166365aefbe07d28d24e3c10511
Creatordante <111818541+Creatordante@users.noreply.github.com>
2022-09-27 21:51:31 -0500

    Added sprites for the runes

--- Assets/Scripts/Minigames 1/RuneWordle.cs
+++ Assets/Scripts/Minigames 1/RuneWordle.cs
@@ -2,15 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum RuneType
+public enum Runes
 {
-    Star = 0,
-    Circle = 1,
-    Symbol = 2,
-
+    RuneHalcon = 0,
+    RuneCulebra = 1,
+    RunePerdiz = 2,
+    RuneZamut = 3,
+    RuneRepelon = 4
 }
 
 public class RuneWordle : Minigame
 {
-    //array int [4]
+    int[] _runeSequence = new int[4];
+    int _randNumber;
+    
+    public override void StartMinigame()
+    {
+        base.StartMinigame();
+        RandomRuneSequence();
+    }
+
+    private void RandomRuneSequence()
+    {
+        _randNumber = Random.Range(0, 5);
+
+    }
+
 }
